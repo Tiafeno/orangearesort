b0a33bd08940aa4cff1c15ac6de10667ded424f9
Tiafeno <tiafenofnel@gmail.com>
2019-09-05 23:52:08 +0300

    Add javascript image load script

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,55 @@
-(function($) {
-	$(document).ready(function(){
+(function ($) {
+	$(document).ready(function () {
 
 		// responsive nav
 		var responsiveNav = $('#toggle-nav');
 		var navBar = $('.nav-bar');
-	
-		responsiveNav.on('click',function(e){
+
+		responsiveNav.on('click', function (e) {
 			e.preventDefault();
 			console.log(navBar);
 			navBar.toggleClass('active')
 		});
-	
+
 		// pseudo active
-		if($('#docs').length){
+		if ($('#docs').length) {
 			var sidenav = $('ul.side-nav').find('a');
-			var url = window.location.pathname.split( '/' );
-			var url = url[url.length-1];
-			
-			sidenav.each(function(i,e){
+			var url = window.location.pathname.split('/');
+			var url = url[url.length - 1];
+
+			sidenav.each(function (i, e) {
 				var active = $(e).attr('href');
-	
-				if(active === url){
+
+				if (active === url) {
 					$(e).parent('li').addClass('active');
 					return false;
 				}
 			});
 		}
-	
+
+		var imgs = document.images,
+			len = imgs.length,
+			counter = 0;
+
+		[].forEach.call( imgs, function( img ) {
+			img.addEventListener( 'load', function () {
+				counter++;
+				if ( counter === len ) {
+					console.log( 'All images loaded!' );
+				}
+			}, false );
+		} );
+
+		$(window).bind("load", function() {
+			// code here
+			console.log('Image bin load');
+		 });
+
+
 	});
 })(jQuery);
 
-hljs.configure({tabReplace: '  '});
+hljs.configure({
+	tabReplace: '  '
+});
 hljs.initHighlightingOnLoad();
