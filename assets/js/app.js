b7f169e31ca9a66b91fb611e260a189d50735aab
Tiafeno <tiafenofnel@gmail.com>
2019-10-22 19:01:54 +0300

    Update version to 0.0.5

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,38 @@
 (function ($) {
     setLoading(true);
+	$.fn.organizeHeaderPosition = function () {
+        var width = $(this).innerWidth();
+        var imgHeight = 80;
+
+        // Permet de corriger la forme wave des sections (en haut)
+        $('.section-header-top').find('img').each(function (index, el) {
+            var imgWidth = $(el).innerWidth();
+            if (width > imgWidth) {
+                var finalHeight = (width * imgHeight) / imgWidth;
+                $(el).css({
+                    'width': width,
+                    'height': parseInt(finalHeight, 10) + 1,
+                    'top': -(parseInt(finalHeight, 10) - 1) + 'px'
+                });
+            }
+        });
+
+        /**
+         * Permet de corriger la forme wave des sections (en bas)
+         */
+        $('.section-header-bottom').find('img').each(function (index, el) {
+            var imgWidth = $(el).innerWidth();
+            if (width > imgWidth) {
+                var finalHeight = (width * imgHeight) / imgWidth;
+                $(el).css({
+                    'width': width,
+                    'height': parseInt(finalHeight, 10) + 1,
+                    'bottom': -(parseInt(finalHeight, 10) - 1) + 'px'
+                });
+            }
+        });
+	}
+	
     $(document).ready(function () {
 
         // responsive nav
@@ -8,7 +41,6 @@
 
         responsiveNav.on('click', function (e) {
             e.preventDefault();
-            console.log(navBar);
             navBar.toggleClass('active')
         });
 
@@ -32,19 +64,19 @@
             // the img to watch
             var $img = $("img.preloading");
             if (!!$img.length && $img[0].complete) {
-                // clear the timer
-                clearInterval(loop);
-                _fixSectionHeader();
+				$('#content').organizeHeaderPosition();
                 setLoading(false);
+				clearInterval(loop);
             }
 
         }, 30);
     });
 
     $(window).resize(function () {
-        _fixSectionHeader();
+        $('#content').organizeHeaderPosition();
     });
-    
+	
+	
     function setLoading(value) {
         var preload = $('.preload-backdrop');
         if (value) {
@@ -58,42 +90,6 @@
                 display: 'none'
             });
         }
-    }
-
-    function _fixSectionHeader() {
-        var container = $('#content');
-        var width = container.innerWidth();
-
-        // Permet de corriger la forme wave des sections (en haut)
-        $('.section-header-top').find('img').each(function (index, el) {
-            var imgHeight = $(el).innerHeight();
-            var imgWidth = $(el).innerWidth();
-            if (width > imgWidth) {
-                var finalHeight = (width * imgHeight) / imgWidth;
-                $(el).css({
-                    'width': width,
-                    'height': parseInt(finalHeight, 10) + 1,
-                    'top': -(parseInt(finalHeight, 10) - 1) + 'px'
-                });
-            }
-        });
-
-        /**
-         * Permet de corriger la forme wave des sections (en bas)
-         */
-        $('.section-header-bottom').find('img').each(function (index, el) {
-            var imgHeight = $(el).innerHeight();
-            var imgWidth = $(el).innerWidth();
-            if (width > imgWidth) {
-                var finalHeight = (width * imgHeight) / imgWidth;
-                $(el).css({
-                    'width': width,
-                    'height': parseInt(finalHeight, 10) + 1,
-                    'bottom': -(parseInt(finalHeight, 10) - 1) + 'px'
-                });
-            }
-        });
-
     }
 
 
